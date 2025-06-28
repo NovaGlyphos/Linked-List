798dfdcd8becb9b63f61d6d9412854674386849d
Ayush <ag13015002@gmail.com>
2025-06-28 23:47:16 +0530

    initialized frontend and basic routing

---
+++ frontend/fe/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
+import Login from './components/Login'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
