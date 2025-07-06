770acbc58a6abe26995aa98a348bbf298c57082f
Ayush <ag13015002@gmail.com>
2025-07-06 21:32:46 +0530

    feat:Request Page (accept and reject)

--- frontend/fe/src/components/Requests.jsx
+++ frontend/fe/src/components/Requests.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useDispatch, useSelector } from 'react-redux';
-import { addRequest } from '../utils/requestSlice';
-
+import { useDispatch, useSelector } from "react-redux";
+import { addRequest } from "../utils/requestSlice";
 
 const Requests = () => {
-
   const dispatch = useDispatch();
-  const requests = useSelector((store)=>store.request)
-  const getRequest = async ()=>{
-    try{
-      const res = await axios.get(BASE_URL+"/user/requests/received",{withCredentials:true});
+  const requests = useSelector((store) => store.request);
+  const handleRequest = async (status,id) => {
+    const res = await axios.post(BASE_URL+"/request/review/"+status+"/"+id,{},{withCredentials:true});
+    console.log(res.data);
+  }
+  const getRequest = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/received", {
+        withCredentials: true,
+      });
       dispatch(addRequest(res.data.connectionReceived));
-    }
-    catch(err){
+    } catch (err) {
       console.log(err.message);
     }
-  }
-  useEffect(()=>{
+  };
+  useEffect(() => {
     getRequest();
-  },[]);
+  }, []);
 
   if (!requests) return <div className="text-center mt-4">Loading...</div>;
 
@@ -52,57 +55,27 @@ const Requests = () => {
       </h1>
       <ul className="list bg-base-100 rounded-box shadow-md mx-96">
         {requests.map((req) => {
-          
           return (
-            <li key={req.fromUserId._id} className="list-row">
+            <li key={req.fromUserId._id} className="list-row flex justify-around">
               <div>
                 <div>
-                  {req.fromUserId.firstName +
-                    " " +
-                    req.fromUserId.lastName}
+                  {req.fromUserId.firstName + " " + req.fromUserId.lastName}
                 </div>
               </div>
-              {/* <button className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                  >
-                    <path d="M6 3L20 12 6 21 6 3z"></path>
-                  </g>
-                </svg>
-              </button>
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  className="size-[1.2em]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    strokeLinejoin="round"
-                    strokeLinecap="round"
-                    strokeWidth="2"
-                    fill="none"
-                    stroke="currentColor"
-                  >
-                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                  </g>
-                </svg>
-              </button> */}
+              <div>
+                <button className="btn btn-circle" onClick={()=>handleRequest("rejected",req._id)}>
+                  ❌
+                </button>
+                <button className="btn btn-circle" onClick={()=>handleRequest("accepted",req._id)}>
+                  ✔
+                </button>
+              </div>
             </li>
           );
         })}
       </ul>
     </div>
   );
-}
-
+};
 
 export default Requests;
